82005ad6c7951496d5113a257811801635251430
amilandau <amirlandau5@gmail.com>
2022-08-31 13:32:58 +0300

    commit 1

---
+++ DevOpsTask/my_script.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+def func():
+
+    if len(argv) < 2:
+        return 0
+
+    textToPrint = argv[1]
+
+    if "." in argv[1]:
+        textToPrint = argv[1].rsplit(".", 1)[0]
+
+    f = open(argv[1], "a")
+    f.write(textToPrint)
+    f.close()
+
+if __name__ == "__main__":
+    func()
